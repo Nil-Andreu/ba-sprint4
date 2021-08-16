2cf6aa5709aa3ffffd8ba954d7cc436ecc8a8ee0
Nil Andreu <nilandreug@gmail.com>
2021-08-16 08:58:29 +0200

    Comments

--- src/films.js
+++ src/films.js
@@ -1,5 +1,9 @@
 // Exercise 1: Get the array of all directors.
 function getAllDirectors(array) {
+  /* How I will solve this is by creating a new array directors, and then pushing all the directors of the array parameter
+  * using the map method 
+  */
+
   // Create a directors array
   directors = [];
 
@@ -9,7 +13,6 @@ function getAllDirectors(array) {
     directors.push(i.director);
   });
   let result = directors;
-  console.log('EXERCICE 1 ->', result);
   return result;
 }
 
@@ -17,7 +20,6 @@ function getAllDirectors(array) {
 function getMoviesFromDirector(array, director) {
   // We use the filter method, where for each element of the array (i), we check that its director is equal to the one we are searching
   results = array.filter((i) => i.director == director);
-  console.log('EXERCICE 2 ->', results);
   return results;
 }
 
@@ -28,7 +30,6 @@ function moviesAverageOfDirector(array, director) {
 
   // Now we apply the reduce method
   let sum = results.reduce((sum, result) => {
-    // Anonymous function
     // We are reducing the array with the function of summing, which takes a new value sum and each element of results (result).
     // So we sum each result score
     const new_sum = sum + result.score;
@@ -51,7 +52,7 @@ function orderAlphabetically(array) {
   array.map((i) => {
     new_array.push(i.title);
   });
-  sorted_array = new_array.sort();
+  sorted_array = new_array.sort(); // The default sort will be alphabetic
 
   return sorted_array.slice(0, 20); // To return only the 20 top elements, so we start at the index 0
 }
@@ -71,20 +72,22 @@ function orderByYear(array) {
 
     // In the case that the years are the same
     if ((first.year = last.year)) {
-      // Going to sort by title
+      // Going to sort by title when the years are the same
       if (first.title > last.title) {
         return 1;
       }
 
       if (first.title < last.title) {
         return -1;
-      } else {
+      } 
+      else {
         return 0;
       }
     }
   });
 
   sorted_array = [];
+
   // And now we only pass the titles ordered
   new_array.map((i) => {
     // And now we create an object with the information we need to test from new_arrat
@@ -98,7 +101,7 @@ function orderByYear(array) {
 function moviesAverageByCategory(array, genre) {
   // Obtain the movies from the genre we need
   let movies_genre = array.filter((i) => {
-    // As there can be many genre in a film, we will loop for each one
+    // As there can be many genre in a film, we will loop for each one (could use the for...of iteration)
     for (let j = 0; j < i.genre.length; j++) {
       if (i.genre[j] == genre) {
         return true; // Should be catched for the filter
@@ -133,20 +136,22 @@ function moviesAverageByCategory(array, genre) {
 
 // Exercise 7: Modify the duration of movies to minutes
 function hoursToMinutes(array) {
-  // Create a new array which will have the old array
+  /* Create a new array which will have the old array
+  * And then we will loop for the array and check the possible times that a film can have: hours+minutes, only hours or only minutes
+  */
   new_array = []
 
   for (let i = 0; i < array.length; i++) {
     // Create each element of the new array (new place in memory), where the duration will be set to 0 by default
     new_array.push({title: array[i].title, year: array[i].year, director: array[i].director, duration: 0, genre: array[i].genre, score: array[i].score})
 
-    // We use the include method, which is for strings
+    // We use the include method, which is for strings to check for a substring
     if (array[i].duration.includes('h')) {
       if (array[i].duration.includes('min')) {
         // It is like 2h 45min
         let [hours, minutes] = array[i].duration.split(' ');
-        let [number_hours, letter_hours] = hours.split('h');
-        let [number_minutes, letter_minutes] = minutes.split('m');
+        let [number_hours, letter_hours] = hours.split('h'); // So we would obtain 2 and h
+        let [number_minutes, letter_minutes] = minutes.split('m'); // So we obtain 45 and min
 
         new_array[i].duration =
           Number(number_hours) * 60 + Number(number_minutes);
@@ -166,6 +171,9 @@ function hoursToMinutes(array) {
 
 // Exercise 8: Get the best film of a year
 function bestFilmOfYear(array, year) {
+  /* We will loop of the array, and check if the element if from the year we are searching
+  *  And if it is from the year we are searching, we will compare its score with the best score we have
+  */
   best_film = [];
   best_score = 0;
   for (let i = 0; i < array.length; i++) {
